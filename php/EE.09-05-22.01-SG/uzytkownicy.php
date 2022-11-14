ba565f57f3b8342543b37f59ea1811e2e0caa9cc
Jolanta Gryko <jgryko@gmail.com>
2022-11-14 11:38:38 +0100

    dodane zapytania

---
+++ php/EE.09-05-22.01-SG/uzytkownicy.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Portal społecznościowy</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='styl5.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <header id="leftHeader">
+        <h2>Nasze osiedle</h2>
+    </header>
+    <header id="rightHeader">
+        <?php
+            echo "skrypt 1";
+        ?>
+    </header>
+    <section id="leftSection">
+        <h3>logowanie</h3>
+        <form method="POST">
+            <p>Login</p>
+            <input type="text" name="" id="">
+        </form>
+    </section>
+    <section id="rightSection"></section>
+    <footer></footer>
+</body>
+</html>
