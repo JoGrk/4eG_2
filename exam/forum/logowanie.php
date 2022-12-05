86fccecf91377d199ffd7c1a665e38c2fa5ed30b
Jola Gryko <jgryko@gmail.com>
2022-12-05 10:06:07 +0100

    fff

---
+++ exam/forum/logowanie.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Forum o psach</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='styl4.css'>
+</head>
+<body>
+    <header id="banner">
+        <h1>Forum wielbicieli psów</h1>
+    </header>
+    <section id="leftSection">
+        <img src="obraz-skala.jpg" alt="foksterier">
+    </section>
+    <section id="rightSection_1">
+        <h2>Zapisz się</h2>
+        <form method="post">
+            Login: <input type="text" id="login" name="login"><br>
+            Hasło: <input type="password" id="password" name="password"><br>
+            Powtórz hasło: <input type="password" id="repeatPassword" name="repeatPassword"><br>
+            <input type="submit" value="Zapisz" id="button">
+        </form>
+        <?php
+          
+
+        ?>
+    </section>
+    <section id="rightSection_2">
+        <h2>Zapraszamy wszystkich</h2>
+        <ol>
+            <li>Właścicieli psów</li>
+            <li>Weterynarzy</li>
+            <li>Tych, co chcą kupić psa</li>
+            <li>Tych, co lubią psy</li>
+        </ol>
+        <a href="regulamin.html">Przeczytaj regulamin forum</a>
+    </section>
+    <footer id="footer">
+        Stronę wykonał: 00000000000
+    </footer>
+</body>
+</html>
